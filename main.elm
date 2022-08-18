423cea13da4f745638e85e3815de3209ab7b7128
Konwoh <konstantin.wohllaub@gmail.com>
2022-08-18 14:13:13 +0200

    init mit Http.get und URL erweitert

--- main.elm
+++ main.elm
@@ -7,6 +7,18 @@ main =
         , update = update
         }
 
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Loading
+  , Http.get
+      { url = "https://cloud.informatik.uni-halle.de/s/pSKk3izyLRQ84C9/download/supermarket_sales%20-%20Sheet1.csv"
+      , expect = Http.expectString GotText
+      }
+  )
+
+
+
 view : Model -> Html.Html Msg
 view model =
     Html.div [] []

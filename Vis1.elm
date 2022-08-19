86100f3cbca6a3cafe7b98bab62ba4bab7b0a163
Konwoh <konstantin.wohllaub@gmail.com>
2022-08-19 14:12:24 +0200

    Vis1: import benötigter module

---
+++ Vis1.elm
@@ -0,0 +1,9 @@
+import Axis
+import Html exposing (Html, text)
+import Scale exposing (ContinuousScale)
+import Statistics
+import TypedSvg exposing (circle, g, rect, style, svg, text_)
+import TypedSvg.Attributes exposing (class, fontFamily, fontSize, textAnchor, transform, viewBox)
+import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, y)
+import TypedSvg.Core exposing (Svg)
+import TypedSvg.Types exposing (AnchorAlignment(..), Length(..), Transform(..), px)

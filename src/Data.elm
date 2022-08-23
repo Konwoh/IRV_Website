7e765c52804ed58414ce3ed66036ab0447c12aa8
Konwoh <konstantin.wohllaub@gmail.com>
2022-08-23 13:14:56 +0200

    Data: aktualisieren des Decoder und der Datentypes

--- src/Data.elm
+++ src/Data.elm
@@ -28,17 +28,17 @@ type Msg
 type alias Sale =
   { invoice_ID : String
   , branch : Branch
-  , city : String
-  , customer_type : String
-  , gender : String
-  , product_line : String
+  , city : City
+  , customer_type : Customer_type
+  , gender : Gender
+  , product_line : Product_line
   , unit_price : Float
   , quantity : Int
   , tax : Float
   , total : Float
   , date : String
   , time : String
-  , payment : String
+  , payment : Payment
   , cogs : Float
   , gross_margin_percentage : Float
   , gross_income : Float
@@ -49,13 +49,14 @@ type Branch
     = A
     | B
     | C
-    | Nope
+    | None
 
 
 type City
     = Yangon
     | Naypyitaw
     | Mandalay
+    | NoCity
 
 type Customer_type
     = Normal
@@ -65,17 +66,20 @@ type Gender
     = Male
     | Female
 
+
 type Product_line
     = Health_and_beauty
     | Electronic_accessories
     | Home_and_lifestyle
     | Sports_and_travel
     | Food_and_beverages
+    | NoProductLine
 
 type Payment 
     = Ewallet
     | Cash
     | Credit_card
+    | NoPayment
 
 type Attributes
     = Unit_pric
@@ -145,21 +149,32 @@ salesDecoder : Decoder Sale
 salesDecoder =
     Decode.into Sale
         |> Decode.pipeline (Decode.field "Invoice_ID" Decode.string)
-        |> Decode.pipeline (Decode.field "Branch" (Decode.map (\i -> case i of  "A" -> A 
-                                                                                "B" -> B
-                                                                                "C" -> C 
-                                                                                _ -> Nope) Decode.string))
-        |> Decode.pipeline (Decode.field "City" Decode.string)
-        |> Decode.pipeline (Decode.field "Customer Type" Decode.string)
-        |> Decode.pipeline (Decode.field "Gender" Decode.string)
-        |> Decode.pipeline (Decode.field "Product line" Decode.string)
+        |> Decode.pipeline (Decode.field "Branch" (Decode.map (\i -> case i of "A" -> A 
+                                                                               "B" -> B
+                                                                               "C" -> C 
+                                                                               _ -> None) Decode.string))
+        |> Decode.pipeline (Decode.field "City" (Decode.map (\i -> case i of "Yangon" -> Yangon
+                                                                             "Naypyitaw" -> Naypyitaw 
+                                                                             "Mandalay" -> Mandalay 
+                                                                             _ -> NoCity) Decode.string))
+        |> Decode.pipeline (Decode.field "Customer Type" (Decode.map (\i -> if i == "Normal" then Normal else Member) Decode.string))
+        |> Decode.pipeline (Decode.field "Gender" (Decode.map (\i -> if i == "Male" then Male else Female) Decode.string))
+        |> Decode.pipeline (Decode.field "Product line" (Decode.map (\i -> case i of "Health_and_beauty" -> Health_and_beauty 
+                                                                                     "Electronic_accessories" -> Electronic_accessories 
+                                                                                     "Home_and_lifestyle" -> Home_and_lifestyle 
+                                                                                     "Sports_and_travel" -> Sports_and_travel 
+                                                                                     "Food_and_beverages" -> Food_and_beverages 
+                                                                                     _ -> NoProductLine) Decode.string))
         |> Decode.pipeline (Decode.field "Unit price" Decode.float)
         |> Decode.pipeline (Decode.field "quantity" Decode.int)
         |> Decode.pipeline (Decode.field "tax" Decode.float)
         |> Decode.pipeline (Decode.field "Total" Decode.float)
         |> Decode.pipeline (Decode.field "date" Decode.string)
         |> Decode.pipeline (Decode.field "time" Decode.string)
-        |> Decode.pipeline (Decode.field "payment" Decode.string)
+        |> Decode.pipeline (Decode.field "payment" (Decode.map (\i -> case i of "Ewallet" -> Ewallet 
+                                                                                "Cash" -> Cash 
+                                                                                "Credit_card" -> Credit_card 
+                                                                                _ -> NoPayment) Decode.string))
         |> Decode.pipeline (Decode.field "cogs" Decode.float)
         |> Decode.pipeline (Decode.field "gross margin percentage" Decode.float)
         |> Decode.pipeline (Decode.field "gross income" Decode.float)

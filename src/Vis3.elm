d34c55c2d5df48d5115fef43715fa72c98c29738
Konwoh <konstantin.wohllaub@gmail.com>
2022-08-30 13:58:09 +0200

    Vis3: Datumliste erstellt

---
+++ src/Vis3.elm
@@ -0,0 +1,10 @@
+createDateList : List String
+createDateList =
+    let
+        start =
+            fromCalendarDate 2019 Jan 1
+
+        end =
+            fromCalendarDate 2019 Mar 30
+    in
+    List.map Date.toIsoString (range Day 1 start end)
